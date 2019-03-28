db9432006194df66fcd0f251c9914dd2804864d0
X-Vector <30379192+X-Vector@users.noreply.github.com>
2019-03-28 16:13:43 +0200

    Update X0R.py

--- X0R.py
+++ X0R.py
@@ -1,7 +1,5 @@
-import sys
-import base64
-import platform,os
-
+import os,platform,sys
+from urllib2 import *
 clear = ""
 if "Windows" in platform.system():
 	clear = "cls"
@@ -9,6 +7,12 @@ if "Linux" in platform.system():
 	clear = "clear"
 os.system(clear)
 is_windows = sys.platform.startswith('win')
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(4. / 100)
 
 # Console Colors
 if is_windows:
@@ -54,106 +58,91 @@ def banner():
 [ Facebook: facebook.com/X.Vector1 ]\033[95m
     """ % (R, W,R))
 
-
-# Xor Function
-
-def xor_base64():
-    try:
-        plaintext = raw_input("\033[94mEnter Plaintext (key) : \033[94m")
-        ciphertext = raw_input("\033[94mEnter Cipher : \033[94m")
-        ciphertext2 = bytearray(base64.b64decode(ciphertext))
-
-        xor_key = []
-        for i in range(len(plaintext)):
-            xor_key.append(chr(ciphertext2[i] ^ ord(plaintext[i])))
-
-        key = "".join(xor_key)
-
-        print "\033[92m[+] Yor Key is : \033[92m",key
-
-        for i in range(len(ciphertext2)):
-        	ciphertext2[i] ^= ord(key[i%len(key)])
-
-        print "\033[91m[+] Your Plaintext : \033[91m",ciphertext2
-
-    except TypeError:
-        print "\n\033[91m[-] incorrect Encrypted Text\033[91m"
-    except IndexError:
-        print "\n\033[91m[-] Enter A Part From Plaintext\033[91m"
-    except KeyboardInterrupt:
-        print "\n\033[91m[-] GoodBye\033[91m"
-def xor_file():
-    try:
-        plaintext = raw_input("\033[94mEnter Plaintext (key) : \033[94m")
-        file_name = raw_input("\033[93mEnter Encrypted File : \033[93m")
-        ciphertext = file(file_name).read()
-        ciphertext2 = bytearray()
-        for i in ciphertext:
-            ciphertext2.append(i)
-
-        xor_key = []
-        for i in range(len(plaintext)):
-            xor_key.append(chr(ciphertext2[i] ^ ord(plaintext[i])))
-
-        key = "".join(xor_key)
-
-        print "\033[92m[+] Yor Key is : \033[92m",key
-
-        for i in range(len(ciphertext)):
-        	ciphertext2[i] ^= ord(key[i%len(key)])
-        print "\033[91m[+] Your Plaintext : \033[91m",ciphertext2
-    except IOError:
-        print "\n\033[91m[-] The File",file_name,"Not Exist\033[91m"
-    except IndexError:
-        print "\n\033[91m[-] Enter Correct Plaintext (Key)\033[91m"
-    except KeyboardInterrupt:
-        print "\n\033[91m[-] GoodBye\033[91m"
-def xor_hex():
-    try:
-        plaintext = raw_input("\033[94mEnter Plaintext (key) : \033[94m")
-        ciphertext = raw_input("\033[94mEnter Cipher : \033[94m")
-        ciphertext2 = bytearray(ciphertext.decode("hex"))
-
-        xor_key = []
-        for i in range(len(plaintext)):
-            xor_key.append(chr(ciphertext2[i] ^ ord(plaintext[i])))
-
-        key = "".join(xor_key)
-
-        print "\033[92m[+] Yor Key is : \033[92m",key
-
-        for i in range(len(ciphertext2)):
-        	ciphertext2[i] ^= ord(key[i%len(key)])
-
-        print "\033[91m[+] Your Plaintext : \033[91m",ciphertext2
-
-    except TypeError:
-        print "\n\033[91m[-] incorrect Encrypted Text\033[91m"
-    except IndexError:
-        print "\n\033[91m[-] Enter A Part From Plaintext\033[91m"
-    except KeyboardInterrupt:
-        print "\n\033[91m[-] GoodBye\033[91m"
-
-
-
-
 banner()
+
 try:
-    print """
-1 - File Xored
-2 - Base64 String Xored
-3 - Hex String Xored
-    """
-    choose = int(raw_input(">>> "))
-    if choose == 1:
-        xor_file()
-    elif choose == 2:
-        xor_base64()
-    elif choose == 3:
-        xor_hex()
-    else :
-        print "\n\033[91m[-] invalid Choose\033[91m"
+	from functions import *
+	print """
+[1] Known Plain Text Attack
+[2] Many Time Pad Attack
+	"""
+
+	check1 = int(raw_input(">>> "))
+	if check1 == 1:
+		os.system("clear")
+		banner()
+		print """
+\n\033[91m[~] Known Plain Text Attack\033[95m
+
+[1] File Xored
+[2] Base64 String Xored
+[3] Hex String Xored
+		"""
+		choose = int(raw_input(">>> "))
+		if choose == 1:
+			xor_file()
+		elif choose == 2:
+			xor_base64()
+		elif choose == 3:
+			xor_hex()
+		else :
+			print "\n\033[91m[-] invalid Choose\033[91m"
+	elif check1 == 2:
+		os.system("clear")
+		banner()
+		print """
+\n\033[91m[~] Many Time Pad Attack\033[95m
+
+[1] File Xored
+[2] Base64 String Xored
+[3] Hex String Xored
+"""
+		check = int(raw_input(">>> "))
+		if check == 1:
+			ciphers = []
+			number_of_cipher = int(raw_input("How Many File : "))
+			i = 0
+			print ("[~] Enter Path of Files")
+			while(i < number_of_cipher):
+				c = raw_input(">>> ")
+				ciphers.append(c)
+				i = i+1
+			target_cipher = raw_input("Enter Target File : ")
+			MTP_files(ciphers,target_cipher)
+
+		elif check == 2:
+			ciphers = []
+			number_of_cipher = int(raw_input("How Many Cipher : "))
+			print ("[~] Enter Cipher")
+			i = 0
+			while(i < number_of_cipher):
+				c = raw_input(">>> ")
+				ciphers.append(c)
+				i = i+1
+			target_cipher = raw_input("Enter Target Cipher : ")
+			MTP_base64(ciphers,target_cipher)
+
+		elif check == 3:
+			ciphers = []
+			number_of_cipher = int(raw_input("How Many Cipher : "))
+			print ("[~] Enter Cipher")
+			i = 0
+			while(i < number_of_cipher):
+				c = raw_input(">>> ")
+				ciphers.append(c)
+				i = i+1
+			target_cipher = raw_input("Enter Target Cipher : ")
+			MTP_hex(ciphers,target_cipher)
+
 except KeyboardInterrupt:
-    print "\n\033[91m[-] GoodBye\033[91m"
-except:
-    print "\n\033[91m[-] invalid Choose\033[91m"
+	print "\n\033[91m[-] GoodBye\033[91m"
+except IOError:
+	print "\n\033[91m[-] Files Name/Path Wrong\033[91m"
+except TypeError:
+	print "\n\033[91m[-] incorrect Encryption\033[91m"
+except ImportError:
+	slowprint("\n\033[91m[-] Module Not Installed Well \033[91m")
+	slowprint("\n[~] Try \033[93msudo pip install requirement.txt")
+except :
+	print "\n\033[91m[-] invalid Choose\033[91m"
+
